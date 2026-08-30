666ec397ede42aa14e87e3297bb06f09846f7de2
aleksvilly <49431859+aleksvilly@users.noreply.github.com>
2026-08-30 18:44:14 +0300

    Fix Vercel SSR build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 import vue from '@astrojs/vue';
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-
 export default defineConfig({
-  site: isGitHubPages ? 'https://aleksvilly.github.io' : undefined,
-  base: isGitHubPages ? '/visual-slider/' : '/',
+  output: 'server',
+  adapter: vercel(),
   integrations: [vue()],
 });
