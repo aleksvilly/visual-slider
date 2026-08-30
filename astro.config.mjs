c04f87d7ecabbc3ea8800f27d49d2afb74300855
aleksvilly <49431859+aleksvilly@users.noreply.github.com>
2026-08-30 13:23:14 +0300

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
 export default defineConfig({
+  site: 'https://aleksvilly.github.io',
+  base: '/visual-slider',
   integrations: [vue()],
 });
